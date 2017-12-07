3ca2e69fd1a2f062e3f1badfcf252139fbb96727
Mitchell Clements <mitch.clements37@gmail.com>
2017-12-06 17:42:33 -0700

    Add files via upload

---
+++ src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -0,0 +1,31 @@
+package com.google.firebase.udacity.friendlychat;
+
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.facebook.FacebookSdk;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+        mapsFragment = new MapsFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.content, mapsFragment).commit();
+    }
+
+    private MapsFragment mapsFragment;
+
+
+
+}
---
+++ src/main/java/com/google/firebase/udacity/friendlychat/SelectorActivity.java
@@ -0,0 +1,55 @@
+package com.google.firebase.udacity.friendlychat;
+
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+import com.google.firebase.udacity.friendlychat.model.ProfileData;
+
+public class SelectorActivity extends AppCompatActivity {
+
+    private LinearLayout mHikeOption;
+    private LinearLayout mObserverOption;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hideActionBar();
+        setContentView(R.layout.activity_selection);
+
+        mHikeOption = (LinearLayout) findViewById(R.id.hike);
+        mHikeOption.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ProfileData.getInstance().setUserType(ProfileData.USER.HIKER);
+                Intent mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainActivityIntent);
+            }
+        });
+
+        mObserverOption = (LinearLayout) findViewById(R.id.observe);
+        mObserverOption.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ProfileData.getInstance().setUserType(ProfileData.USER.OBSERVER);
+                Intent mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainActivityIntent);
+            }
+        });
+    }
+
+    //WARNING: Make this is called before setContentView()
+    private void hideActionBar() {
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            // Hide ActionBar
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.hide();
+        }
+    }
+}
